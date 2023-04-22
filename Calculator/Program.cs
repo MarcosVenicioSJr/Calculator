37a084d8744b4ce6b68c9aa344e00e449ab93c9a
Marcos Venicio <marcos.venicio1230@hotmail.com>
2023-04-22 11:37:49 -0300

    Switch criado. Mini-projeto terminado.

--- Calculator/Program.cs
+++ Calculator/Program.cs
@@ -2,12 +2,34 @@
 
 Console.Clear();
 
+Console.WriteLine("Digite qual operação você deseja: ");
+string operationMath = Console.ReadLine().ToLower();
+
 Console.WriteLine("Digite o primeiro valor: ");
 double valueOne = double.Parse(Console.ReadLine());
 
 Console.WriteLine("Digite o segundo valor: ");
 double valueTwo = Convert.ToDouble(Console.ReadLine());
 
+
+switch (operationMath)
+{
+    case "soma":
+        Console.WriteLine("O valor é: " + Sum(valueOne, valueTwo));
+        break;
+    case "subtração":
+        Console.WriteLine("O valor é: " + Subtraction(valueOne, valueTwo));
+        break;
+    case "divisão":
+        Console.WriteLine("O valor é: " + Division(valueOne, valueTwo));
+        break;
+    case "multiplicação":
+        Console.WriteLine("O valor é: " + Multiply(valueOne, valueTwo));
+        break;
+    default:
+        Console.WriteLine("Operação não encontrada.");
+        break;
+}
 
 static double Sum(double v1, double v2)
 {
