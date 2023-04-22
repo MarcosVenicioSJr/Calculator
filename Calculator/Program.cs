a1a36563f503a0ef23551100ba756614fa56bd51
Marcos Venicio <marcos.venicio1230@hotmail.com>
2023-04-22 11:30:08 -0300

    Método de operações criados

--- Calculator/Program.cs
+++ Calculator/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 
 Console.Clear();
+
 Console.WriteLine("Digite o primeiro valor: ");
 double valueOne = double.Parse(Console.ReadLine());
 
@@ -11,4 +12,19 @@ double valueTwo = Convert.ToDouble(Console.ReadLine());
 static double Sum(double v1, double v2)
 {
     return v1 + v2;
+}
+
+static double Subtraction(double v1, double v2)
+{
+    return (v1 - v2);
+}
+
+static double Multiply(double v1, double v2)
+{
+    return v1 * v2;
+}
+
+static double Division(double v1, double v2)
+{
+    return v1 / v2;
 }
