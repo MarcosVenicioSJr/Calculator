51d8512acbecdc6ff078932eb71e4c6df55d95d5
Marcos Venicio <marcos.venicio1230@hotmail.com>
2023-04-22 11:28:08 -0300

    Método Sum criado.

--- Calculator/Program.cs
+++ Calculator/Program.cs
@@ -6,3 +6,9 @@ double valueOne = double.Parse(Console.ReadLine());
 
 Console.WriteLine("Digite o segundo valor: ");
 double valueTwo = Convert.ToDouble(Console.ReadLine());
+
+
+static double Sum(double v1, double v2)
+{
+    return v1 + v2;
+}
